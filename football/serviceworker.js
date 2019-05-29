d62aec496219da6d81766f9932502ae79d9a18ec
Junyu <zhujunyu.njcn@gmail.com>
2019-05-29 22:07:45 +0800

    Add new url input

--- football/serviceworker.js
+++ football/serviceworker.js
@@ -1,6 +1,6 @@
 ﻿console.log("This is service worker talking!");
 
-var cacheName = 'blazor-pwa-sample-v13';
+var cacheName = 'blazor-pwa-sample-v14';
 var filesToCache = [
     './',
     //Html and css files
