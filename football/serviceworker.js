286a1752c496ac207aed030843fe12ade58d8cf8
Junyu <zhujunyu.njcn@gmail.com>
2019-05-30 19:31:46 +0800

    Fix 0 guess error

--- football/serviceworker.js
+++ football/serviceworker.js
@@ -1,6 +1,6 @@
 ﻿console.log("This is service worker talking!");
 
-var cacheName = 'blazor-pwa-sample-v18';
+var cacheName = 'blazor-pwa-sample-v20';
 var filesToCache = [
     './',
     //Html and css files
